a550a23559e93ca8584bbb2289fad706171535e9
CppEnjoyer69 <rafalsawczyszyn@gmail.com>
2024-05-21 19:07:48 +0200

    added geo dirs

--- panorama.py
+++ panorama.py
@@ -1,10 +1,9 @@
 import cv2
 
-Dirs = {
-    1 : "EAST",
-    2 : "SOUTH",
-    3 : "WEST"
-}
+
+Dirs = ['E', 'S', 'W']
+
+m_Dirs = ['NE', 'SE', 'SW', 'NW']
 
 cap = cv2.VideoCapture(2)
 cap.set(cv2.CAP_PROP_FPS, 30)
@@ -34,18 +33,29 @@ def click_event(event, x, y, flags, params):
 
     if event == cv2.EVENT_LBUTTONDOWN:
         font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(image, "NORTH", (x - 50,y - 30), font,
-                        1, (0, 0, 255), 2)
+        cv2.putText(image, "N", (x - 8,y - 30), font,
+                        1, (0, 30, 255), 2)
         cv2.putText(image, "|", (x, y), font,
                         1, (255, 0, 255), 2)
         pos = x
-        for i in range(1,4):
-            pos += step 
+       
+        for dir in Dirs:
+            pos += step
             pos %= panoWidth
-            cv2.putText(image, Dirs.get(i), (pos - 50, y - 30), font,
-                        1, (0, 0, 255), 2)
+            cv2.putText(image, dir, (pos - 8, y - 30), font,
+                        1, (255, 80, 0), 2)
             cv2.putText(image, "|", (pos, y), font,
                             1, (255, 0, 255), 2)
+            
+        m_pos = int(x + step/2)
+        for dir in m_Dirs:
+            cv2.putText(image, dir, (m_pos - 12, y - 30), font,
+                        0.6, (255, 150, 0), 2)
+            cv2.putText(image, "|", (m_pos, y), font,
+                            0.6, (255, 0, 255), 2)
+            m_pos += step
+            m_pos %= panoWidth
+            
             
         cv2.imwrite('text.jpg', image)
         cv2.destroyAllWindows()
